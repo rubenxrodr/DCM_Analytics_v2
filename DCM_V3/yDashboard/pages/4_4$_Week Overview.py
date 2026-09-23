1d4f2c7f9a0d0cd0697def32615d03c202842df1
rubenxrodr <rubenxrodr@gmail.com>
2026-09-22 23:10:28 -0700

    Update 4_4$_Week Overview.py

--- DCM_V3/yDashboard/pages/4_4$_Week Overview.py
+++ DCM_V3/yDashboard/pages/4_4$_Week Overview.py
@@ -363,8 +363,10 @@ st.divider()
 st.header("Defensive Process")
 
 st.caption(
-    "How frequently our defensive behaviors occurred across defensive possessions. \n
-    NOTE: THIS ONLY CONSIDERS COMPLETED DEFENSIVE POSSESSIONS. So if we got a deflection but a play got blown dead - it doesn't appear here, same thing with giving up middle or paint touch"
+    "How frequently our defensive behaviors occurred across defensive possessions."
+)
+st.caption(
+    "NOTE: THIS ONLY CONSIDERS COMPLETED DEFENSIVE POSSESSIONS. So if we got a deflection but a play got blown dead - it doesn't appear here, same thing with giving up middle or paint touch"
 )
 
 
